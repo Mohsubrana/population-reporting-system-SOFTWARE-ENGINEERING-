a23b6ab1adb26d7dcac44d81a55a8d49c03cec86
Mohsubrana <ranam2@roehampton.ac.uk>
2024-07-26 23:48:09 +0100

    Add files via upload

--- world.sql
+++ world.sql
@@ -1,4 +1,4 @@
--- Create the Country table first
+
 CREATE TABLE IF NOT EXISTS Country (
   Code CHAR(3) NOT NULL,
   Name VARCHAR(52) NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS Country (
   PRIMARY KEY (Code)
 );
 
--- Create the City table next
+
 CREATE TABLE IF NOT EXISTS City (
   ID INT NOT NULL AUTO_INCREMENT,
   Name VARCHAR(35) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS City (
   FOREIGN KEY (CountryCode) REFERENCES Country(Code)
 );
 
--- Create the CountryLanguage table
+
 CREATE TABLE IF NOT EXISTS CountryLanguage (
   CountryCode CHAR(3) NOT NULL,
   Language VARCHAR(30) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS CountryLanguage (
   FOREIGN KEY (CountryCode) REFERENCES Country(Code)
 );
 
--- Insert data into Country table
+
 INSERT INTO Country (Code, Name, Continent, Region, SurfaceArea, IndepYear, Population, LifeExpectancy, GNP, GNPOld, LocalName, GovernmentForm, HeadOfState, Capital, Code2)
 VALUES
 ('AFG', 'Afghanistan', 'Asia', 'Southern and Central Asia', 652090.00, 1919, 22720000, 45.9, 5976.00, NULL, 'Afghanistan', 'Islamic Republic', 'Hamid Karzai', 1, 'AF'),
@@ -47,7 +47,7 @@ VALUES
 ('USA', 'United States', 'North America', 'Northern America', 9631420.00, 1776, 327167434, 78.6, 20412850.00, 19485394.00, 'United States', 'Federal Republic', 'Donald Trump', 3813, 'US'),
 ('BRA', 'Brazil', 'South America', 'South America', 8515767.00, 1822, 209288278, 75.7, 2055508.00, 2112107.00, 'Brasil', 'Federal Republic', 'Jair Bolsonaro', 3633, 'BR');
 
--- Insert data into City table
+
 INSERT INTO City (Name, CountryCode, District, Population)
 VALUES
 ('Kabul', 'AFG', 'Kabol', 1780000),
@@ -55,7 +55,7 @@ VALUES
 ('New York', 'USA', 'New York', 8175133),
 ('Rio de Janeiro', 'BRA', 'Rio de Janeiro', 6748000);
 
--- Insert data into CountryLanguage table
+
 INSERT INTO CountryLanguage (CountryCode, Language, IsOfficial, Percentage)
 VALUES
 ('AFG', 'Pashto', 'T', 35.0),
